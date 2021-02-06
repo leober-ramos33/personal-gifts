f55357dd70b8faea47ad6b47056752ead7f88613
Leober Ramos <leober.ramos33@gmail.com>
2021-02-05 23:58:48 -0400

    Initial commit

---
+++ birthdays/elained/15/js/gift.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const envelope = document.querySelector(".envelope");
+    const envelopeTop = document.querySelector(".envelope .top");
+    const envelopeModal = document.querySelector(".envelope .modal");
+
+    // Event Listeners
+    envelope.addEventListener("click", () => {
+        envelopeTop.classList.add("open");
+
+        setTimeout(() => {
+            window.location = "/envelope.html";
+        }, "400");
+    });
+});
